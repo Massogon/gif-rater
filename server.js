ffd26246e717175da7779ac6464f6f6db3b0c10e
wol42verine <49424009+wol42verine@users.noreply.github.com>
2024-07-25 16:50:24 -0700

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
-const giphyRoutes = require('controllers/gifRoutes.js'); // Import the Giphy routes
+const giphyRoutes = require('./controllers/gifRoutes.js'); // Import the Giphy routes
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
