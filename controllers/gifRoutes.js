214ac7f2a4385869a7d70582f0ff7dfb654315cc
Eduardo Velez <166083895+Massogon@users.noreply.github.com>
2024-07-25 21:08:46 -0700

    Add files via upload

--- controllers/gifRoutes.js
+++ controllers/gifRoutes.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { fetchGifById } = require('../controllers/api/gifController');
-const gifController = require('controllers/api/gifController.js'); // Note the './' for same directory
+const { fetchGifById } = require('../controllers/giphyController');
 
 // Route to fetch GIF by index
-router.get('/gif/:id', gifController.fetchGifById);
+router.get('/gif/:id', fetchGifById);
 
 module.exports = router;
--- seeds/seed.js
+++ seeds/seed.js
@@ -2,6 +2,7 @@ const sequelize = require('../config/connection');
 const { User, Project } = require('../models');
 
 const userData = require('./userData.json');
+const gifdata = reuquire('./gifData.json');
 const projectData = require('./projectData.json');
 
 const seedDatabase = async () => {
