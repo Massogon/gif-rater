80febfe4d4d8a90490adee5e87c95886568e0854
Eduardo Velez <166083895+Massogon@users.noreply.github.com>
2024-07-25 20:04:45 -0700

    Add files via upload

---
+++ config/storingGifs.js
@@ -0,0 +1,17 @@
+const gifIds = [
+    'lkibHaGO1xmJXapEdq',
+    'FIyOndr9jvel8vTHLH',
+    '2zoFg7OGkcS82CPK44',
+    't7tGsalcWtUvNSpQ9V',
+    'XpFcYeXZbRWeob95oI',
+    'Ri7d8I18cto2jufOKc',
+    'Ri7d8I18cto2jufOKc',
+    'Ri7d8I18cto2jufOKc',
+    'd1Jfp6LimgUTxK7K',
+    'l2SqcQYL2wj3LJD68',
+    'QLBcA1iilFLESNkmRE',
+    'yvBAuESRTsETqNFlEl'
+  ];
+  
+  module.exports = gifIds;
+  
