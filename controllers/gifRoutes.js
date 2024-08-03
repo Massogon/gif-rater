0b93c48c98bc6de2e307f4a29a3074b53b961c85
MrCows <49926119+MrCows@users.noreply.github.com>
2024-08-03 15:38:50 -0700

    Delete controllers directory

--- controllers/gifRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { fetchGifById } = require('../controllers/api/gifController');
-
-// Route to fetch GIF by index
-router.get('/gif/:id', fetchGifById);
-
-module.exports = router;
